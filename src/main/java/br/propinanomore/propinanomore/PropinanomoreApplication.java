3785c232715328ef189dd301463d4e199e4d9906
Fernando gurgel <fag.gurgel@gmail.com>
2018-11-26 12:19:57 -0400

    init

---
+++ src/main/java/br/propinanomore/propinanomore/PropinanomoreApplication.java
@@ -0,0 +1,12 @@
+package br.propinanomore.propinanomore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropinanomoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropinanomoreApplication.class, args);
+	}
+}
